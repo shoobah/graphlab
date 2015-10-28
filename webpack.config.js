d88a3af9e6daef757802d81020472c6901bfe000
Anders Sjöberg <shoobah@gmail.com>
2015-10-28 20:59:10 +0100

    Döpt om till jsx

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
   ],
   module: {
     loaders: [{
-      test: /\.js$/,
+      test: /\.js[x]$/,
       loaders: ['react-hot', 'babel'],
       include: path.join(__dirname, 'src')
     }]
